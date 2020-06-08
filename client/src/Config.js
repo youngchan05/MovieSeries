d21ce659914d8cd4fdc203b510f5473083841201
younghcan <kimynam1@naver.com>
2020-06-08 17:32:20 +0900

    API 적용

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,5 +1,6 @@
 export const USER_SERVER = 'api/user';
 export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'a18f10ccdaf9291ee972e713f164c99d';
 
 
 export const IMAGE_BASE_URL = 'http://image.tedb.org/t/p';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,8 +1,12 @@
-import React, {useEffect} from 'react'
-import axios from 'axios'
-
+import React, {useEffect} from 'react';
+import {API_URL ,API_KEY} from  '../../../Config';
 function LandingPage() {
-
+    useEffect(()=>{
+        const endpotin = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        fetch(endpotin)
+        .then(response => response.json())
+        .then(response => console.log(response))
+    })
     return (
         <div style={{ width:'100%',margin:'0'}}>
             <div style={{ width:'85%', margin:'1rem auto'}}>
