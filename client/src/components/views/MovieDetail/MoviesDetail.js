2af5da34bd5b3d00412327c1e14798d237d46ce0
younghcan <kimynam1@naver.com>
2020-06-23 17:59:39 +0900

    toggle button Add

--- client/src/components/views/MovieDetail/MoviesDetail.js
+++ client/src/components/views/MovieDetail/MoviesDetail.js
@@ -8,6 +8,7 @@ function MoviesDetail(props) {
     let movieId = props.match.params.movieId
     const [Movie, setMovie] = useState([])
     const [Casts ,setCasts] = useState([])
+    const [ActorToggle , setActorToggle] = useState(false);
     useEffect(() => {
         let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`
         let endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}`
@@ -23,6 +24,9 @@ function MoviesDetail(props) {
             setCasts(response.cast);
         })
     }, [])
+    const toggleActorView = () => {
+        setActorToggle(!ActorToggle);
+    }
     return ( 
         <div>
             {/*header*/}
@@ -42,18 +46,20 @@ function MoviesDetail(props) {
             />
                 <br/>
                 <div style={{display:'flex', justifyContent:'center',margin:'2rem'}}>
-                    <button>Toggle Actor View</button>
+                    <button onClick={toggleActorView}>Toggle Actor View</button>
                 </div>
-                <Row gutter={[16,16]}>
-                    {Casts && Casts.map((Casts , index) => (
-                        <React.Fragment key={index}>
-                            <GrideCards
-                                image={Casts.profile_path ? `${IMAGE_BASE_URL}w500${Casts.profile_path}`:null}
-                                characterName ={Casts.name }
-                            />
-                        </React.Fragment>
-                    ))}
-                </Row>
+                {ActorToggle &&
+                    <Row gutter={[16,16]}>
+                        {Casts && Casts.map((Casts , index) => (
+                            <React.Fragment key={index}>
+                                <GrideCards
+                                    image={Casts.profile_path ? `${IMAGE_BASE_URL}w500${Casts.profile_path}`:null}
+                                    characterName ={Casts.name }
+                                />
+                            </React.Fragment>
+                        ))}
+                    </Row>
+                }
             </div>
         </div>
     )
